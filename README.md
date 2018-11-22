a3e5c6e8490724872f91b7b72dc3583918f2e08a
wjuniori <wjuniori.si@gmail.com>
2018-11-22 13:28:01 -0300

    Criado repositório wjuniori.github.io

---
+++ README.md
@@ -0,0 +1,53 @@
+# wjuniori.github.io
+
+This repository contains the resources and source code of the wjuniori website.
+
+## Social Media
+
+* **[GitHub](https://github.com/wjuniori/)** 
+* **[LinkedIn](https://www.linkedin.com/in/wjuniori/)**
+
+## Preview
+
+* **[Live Preview (GitHub Pages)](https://wjuniori.github.io/)**
+
+## Status
+
+[![dependencies Status](https://david-dm.org/wjuniori/wjuniori.github.io/status.svg)](https://david-dm.org/wjuniori/wjuniori.github.io)
+[![devDependencies Status](https://david-dm.org/wjuniori/wjuniori.github.io/dev-status.svg)](https://david-dm.org/wjuniori/wjuniori.github.io?type=dev)
+
+## Download
+
+* Clone the repo: `git clone https://github.com/wjuniori/wjuniori.github.io.git`
+* [Fork, Clone, or Download on GitHub](https://github.com/wjuniori/wjuniori.github.io)
+
+## Usage
+
+### Basic Usage
+
+After downloading, simply edit the HTML and CSS files included with the website in your favorite text editor to make changes. These are the only files you need to worry about, you can ignore everything else! To preview the changes you make to the code, you can open the `index.html` file in your web browser.
+
+### Advanced Usage
+
+After downloading, run `npm install` and then run `gulp dev` which will open up a preview of the website in your default browser, watch for changes to core website files, and live reload the browser when changes are saved. You can view the `gulpfile.js` to see which tasks are included with the dev environment.
+
+#### Gulp Tasks
+
+- `gulp` the default task that builds everything
+- `gulp dev` browserSync opens the project in your default browser and live reloads when changes are made
+- `gulp html` minifies the HTML files
+- `gulp css` minifies the CSS files
+- `gulp js` minifies the JS files
+- `gulp img` compress image files
+- `gulp vendor` copies dependencies from node_modules to the vendor directory
+- `gulp video` copies video files from ./public/assets/video/ to the ./public/dist/video/ directory
+
+You must have npm and Gulp installed globally on your machine in order to use these features.
+
+## Bugs and Issues
+
+Have a bug or an issue with this website? [Open a new issue](https://github.com/wjuniori/wjuniori.github.io/issues) here on GitHub
+
+## About
+
+This repository/website was created by and is maintained by **[Washington Júnior](https://github.com/wjuniori/)**
---
+++ gulpfile.js
@@ -0,0 +1,122 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+var imageMin = require('gulp-imagemin');
+var concat = require('gulp-concat');
+var htmlMin = require('gulp-htmlmin');
+var notify = require('gulp-notify');
+
+var cssFiles = ['./public/assets/css/**/*.css'];
+var jsFiles = ['./public/assets/js/**/*.js'];
+var imgFiles = [
+  './public/assets/img/**/*.jpg',
+  './public/assets/img/**/*.gif',
+  './public/assets/img/**/*.png',
+  './public/assets/img/**/*.svg',
+  '!./public/assets/img/originals/**/*'
+];
+var videoFiles = ['./public/assets/video/**/*.mp4'];
+var htmlFile = ['./public/assets/*.html'];
+
+// Copy third party libraries from /node_modules into /vendor
+gulp.task('vendor', function() {
+  
+  // Bootstrap
+  gulp.src([
+    './node_modules/bootstrap/dist/**/*',
+    '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+    '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+  ])
+  .pipe(gulp.dest('./vendor/bootstrap'))
+
+  // Font Awesome
+  gulp.src([
+    './node_modules/@fortawesome/fontawesome-free/**/*',
+    '!./node_modules/@fortawesome/fontawesome-free/{less,less/*}',
+    '!./node_modules/@fortawesome/fontawesome-free/{scss,scss/*}',
+    '!./node_modules/@fortawesome/fontawesome-free/.*',
+    '!./node_modules/@fortawesome/fontawesome-free/*.{txt,json,md}'
+  ])
+  .pipe(gulp.dest('./vendor/@fortawesome/fontawesome-free'))
+
+  // jQuery
+  gulp.src([
+    './node_modules/jquery/dist/*',
+    '!./node_modules/jquery/dist/core.js'
+  ])
+  .pipe(gulp.dest('./vendor/jquery'))
+
+});
+
+// Copy video from ./assets/video into ./dist/video
+gulp.task('video', function() {
+  gulp.src(videoFiles)
+    .pipe(gulp.dest('./public/dist/video/'));
+});
+
+// Minify HTML
+gulp.task('html:minify', function() {
+  return gulp.src(htmlFile)
+  .pipe(htmlMin({
+    collapseWhitespace:true
+  }))
+  .on("error", notify.onError("Error: <%= error.message %>"))
+  .pipe(gulp.dest('./'))
+  .pipe(browserSync.stream());
+});
+
+// HTML
+gulp.task('html', ['html:minify']);
+
+// Minify CSS
+gulp.task('css:minify', function() {
+  return gulp.src(cssFiles)
+    .pipe(cleanCSS())
+    .pipe(concat('wjuniori.min.css'))
+    .pipe(gulp.dest('./public/dist/css'))
+    .pipe(browserSync.stream());
+});
+
+// CSS
+gulp.task('css', ['css:minify']);
+
+// Minify JavaScript
+gulp.task('js:minify', function() {
+  return gulp.src(jsFiles)
+    .pipe(uglify())
+    .on("error", notify.onError("Error: <%= error.message %>"))
+    .pipe(concat('wjuniori.min.js'))
+    .pipe(gulp.dest('./public/dist/js'))
+    .pipe(browserSync.stream());
+});
+
+// JS
+gulp.task('js', ['js:minify']);
+
+// Minify Image
+gulp.task('img', function() {
+  return gulp.src(imgFiles)
+    .pipe(imageMin())
+    .pipe(gulp.dest('./public/dist/img/'));
+});
+
+// Default task
+gulp.task('default', ['html', 'css', 'js', 'img', 'video', 'vendor']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+//Dev task
+gulp.task('dev', ['html', 'css', 'js', 'img', 'video', 'vendor', 'browserSync'], function() {
+  gulp.watch(cssFiles, ['css']);
+  gulp.watch(jsFiles, ['js']);
+  gulp.watch(imgFiles, ['img']);
+  gulp.watch(htmlFile, ['html']);
+});
